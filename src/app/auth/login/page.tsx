2e75e970a73b31be0096f06c45b6a6576068a46b
Daniel Guerrero <DanielGDevil@gmail.com>
2025-07-07 19:05:19 -0500

    feat: update dependencies and add Radix UI components
    - Added @radix-ui/react-icons and @radix-ui/themes to dependencies.
    - Updated layout to use Inter font and wrap children in Radix Theme.
    - Created Login page with Radix Button and BookmarkIcon.
    - Created Register page with basic structure.

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@radix-ui/themes";
+import { BookmarkIcon } from "@radix-ui/react-icons";
+
+function Login() {
+  return (
+    <div>
+      Login
+      <Button>
+        <BookmarkIcon /> Bookmark
+      </Button>
+    </div>
+  );
+}
+
+export default Login;
